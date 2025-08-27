be846169d57a7ec366cbf5cf474c05dc781a2f81
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-08-27 15:29:34 +0600

    auth api done

--- src/libs/asyncHandle.ts
+++ src/libs/asyncHandle.ts
@@ -1,14 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
-async function asyncHandler(
-  handler: (req: Request, res: Response, next: NextFunction) => Promise<void>,
-) {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await handler(req, res, next);
-    } catch (error) {
-      next(error);
-    }
+
+type AsyncRequestHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => Promise<any>;
+
+const asyncHandler =
+  (fn: AsyncRequestHandler) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
   };
-}
 
 export default asyncHandler;
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,5 +1,17 @@
 import express from 'express';
+import {
+  getProfile,
+  login,
+  logout,
+  register,
+} from '../controllers/auth.controller';
+import { authenticationHandler } from '../middlewares/authenticationHandler';
 
 const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+router.get('/get-profile', authenticationHandler, getProfile);
+router.post('logout', authenticationHandler, logout);
 
 export default router;
