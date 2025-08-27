d10932c66e0992d166abc6057195695d74eeaed4
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-08-27 15:06:05 +0600

    login user added

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -55,4 +55,58 @@ const register = asyncHandler(async (req, res) => {
   });
 });
 
-export { register };
+const login = asyncHandler(async (req, res) => {
+  const { number, password, database } = req.body;
+
+  // check if all fields are filled
+  if (!database || !number || !password) {
+    throw {
+      status: 400,
+      message: 'All fields are required',
+    };
+  }
+
+  // check if user exists
+  const user = await database.User.findOne({ number });
+  if (!user) {
+    throw {
+      status: 400,
+      message: 'User does not exist',
+    };
+  }
+
+  // check if password is correct
+  if (user.password !== password) {
+    throw {
+      status: 400,
+      message: 'Incorrect password',
+    };
+  }
+
+  // create token
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    config.tokenSecret,
+    {
+      expiresIn: '1d',
+    },
+  );
+
+  // set cookie
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+
+  res.status(200).json({
+    message: 'User logged in successfully',
+    user,
+    token,
+  });
+});
+
+export { login, register };
